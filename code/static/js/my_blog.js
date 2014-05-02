f982876ef092c500d16672b6b118321a0bfbdcf4
cjyfff <1318434601@qq.com>
2014-05-03 03:07:19 +0800

    增加快速提交评论功能

---
+++ code/static/js/my_blog.js
@@ -0,0 +1,8 @@
+$(document).ready(submit_comment);
+
+function submit_comment() {
+	$(document).keypress(function e() {
+		if (e.ctrlKey && e.which == 13 || e.which == 10) {
+			$("#comment_from").submit();
+		});
+}
