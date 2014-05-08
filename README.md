b3d12c24fa8205810ef0ef806488edf55d3492d0
cjyfff <1318434601@qq.com>
2014-05-08 18:26:03 +0800

    删除原来的实现验证码的第三方库，并使用JS来实现验证码

--- README.md
+++ README.md
@@ -6,14 +6,12 @@
 2、python 2.7   
 3、django-contrib-comments    
 4、django-pagination   
-5、markdown     
-6、django-simple-captcha   
-7、PIL   
+5、markdown      
     
 ###目前已经实现的功能：    
 1、博客正文支持markdown语法，支持代码高亮    
 2、博客正文中的段落、图片代码等内容能够按照指定的顺序来显示    
-3、支持评论功能，支持验证码    
+3、支持评论功能，支持验证码(使用JS实现)    
 4、首页实现了分页显示    
 5、实现了RSS功能    
 
--- code/static/js/my_blog.js
+++ code/static/js/my_blog.js
@@ -1,18 +1,85 @@
 $(document).ready(function (){
 	submit_comment();
 	back_to_post();
+	create_code();
+   change_code();
+   control_submit();
+   modify_perview();
 });
 
 function back_to_post(){
+	//实现返回按钮
     $("#back_to_post").click(function () {
         window.history.back();
         });
 }
 
 function submit_comment() {
+	//实现ctrl+enter提交评论
 	$(document).keypress(function e() {
 		if (e.ctrlKey && e.which === 13 || e.which === 10) {
 			$("#comment_from").submit();
 		}
 	});
+}
+
+function change_code() {
+	//更改验证码
+	$("#checkCode").click(function () {
+		create_code();
+	});
+}
+
+var code ;
+function create_code(){
+	//生成验证码
+    code = "";
+    var codeLength = 4;
+    var checkCode = $("#checkCode");
+    var selectChar = new Array(0,1,2,3,4,5,6,7,8,9,'A','B','C','D','E','F','G','H','I','J','K','L',
+                'M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z');
+
+    for(var i=0;i<codeLength;i++){
+        var charIndex = Math.floor(Math.random()*36);
+        code +=selectChar[charIndex];
+    }
+
+    if(checkCode){
+        checkCode.addClass("code");
+        checkCode.val(code);
+    }
+}
+
+function verify_code (){
+	//验证验证码
+    var user_input_code = $("#user_input_code").val().toLowerCase().trim();
+    if(user_input_code.length <=0){
+        return false;
+    } else if(user_input_code != code.toLowerCase()){
+        return false;
+    }
+    return true;
+}
+
+function control_submit() {
+	//当验证码验证成功后，才允许提交评论
+    $("#user_input_code").keyup(function () {
+    	if ($("#user_input_code").val().trim().length>=4) {    
+          if (verify_code()) {
+	           $("#submit_comment").attr("disabled", false);
+	       } else {
+	           $("#valid_faild").text("验证码错误");
+	       }
+    	} else {
+    		 $("#valid_faild").text("");
+    	}
+    });
+}
+
+function modify_perview() {
+	//修改评论模块的perview页面
+	$("#my_preview").find("#id_name").attr("placeholder", "请输入您的昵称");
+	$("#my_preview").find("#id_email").attr("placeholder", "请输入您的邮箱（不会被公开）");
+	$("#my_preview").find("#id_comment").attr("placeholder", "请输入您的评论");
+   $("#my_preview").find("#id_url").parent().remove();
 }
