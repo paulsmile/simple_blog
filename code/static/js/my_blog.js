e75d685cb6663b107ed696caff0cc0fb5d5c113f
cjyfff <1318434601@qq.com>
2014-05-09 20:20:01 +0800

    修改页面布局，修复验证码的bug

--- code/static/js/my_blog.js
+++ code/static/js/my_blog.js
@@ -1,10 +1,12 @@
 $(document).ready(function (){
-	submit_comment();
-	back_to_post();
-	create_code();
-   change_code();
-   control_submit();
-   modify_perview();
+	  var	codeLength = 4;
+		submit_comment();
+		back_to_post();
+		create_code(codeLength);
+	  change_code(codeLength);
+	  control_submit(codeLength);
+	  modify_perview();
+	  initial_blog_page();
 });
 
 function back_to_post(){
@@ -15,26 +17,25 @@ function back_to_post(){
 }
 
 function submit_comment() {
-	//实现ctrl+enter提交评论
-	$(document).keypress(function e() {
-		if (e.ctrlKey && e.which === 13 || e.which === 10) {
-			$("#comment_from").submit();
-		}
-	});
+		//实现ctrl+enter提交评论
+		$(document).keypress(function e() {
+			if (e.ctrlKey && e.which === 13 || e.which === 10) {
+				$("#comment_from").submit();
+			}
+		});
 }
 
-function change_code() {
-	//更改验证码
-	$("#checkCode").click(function () {
-		create_code();
-	});
+function change_code(codeLength) {
+		//更改验证码
+		$("#checkCode").click(function () {
+			create_code(codeLength);
+		});
 }
 
 var code ;
-function create_code(){
+function create_code(codeLength){
 	//生成验证码
     code = "";
-    var codeLength = 4;
     var checkCode = $("#checkCode");
     var selectChar = new Array(0,1,2,3,4,5,6,7,8,9,'A','B','C','D','E','F','G','H','I','J','K','L',
                 'M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z');
@@ -61,25 +62,34 @@ function verify_code (){
     return true;
 }
 
-function control_submit() {
+function control_submit(codeLength) {
 	//当验证码验证成功后，才允许提交评论
     $("#user_input_code").keyup(function () {
-    	if ($("#user_input_code").val().trim().length>=4) {    
-          if (verify_code()) {
-	           $("#submit_comment").attr("disabled", false);
-	       } else {
-	           $("#valid_failed").text("验证码错误");
-	       }
+    	var code_len = $("#user_input_code").val().trim().length
+    	if (code_len < codeLength) {
+    		  $("#valid_failed").text("");
+    	} else if (code_len > codeLength) {
+    		  $("#valid_failed").text("验证码错误");
+    		  $("#submit_comment").attr("disabled", true);
     	} else {
-    		 $("#valid_failed").text("");
-    	}
+		      if (verify_code()) {
+			       $("#submit_comment").attr("disabled", false);
+	        } else {
+	           $("#valid_failed").text("验证码错误");			       
+	        }
+		  }
     });
 }
 
+function initial_blog_page() {
+	  //防止从评论模块的perview页面后退到blog页面时，评论的提交按钮仍然维持disable=“false”状态
+	  $("#submit_comment").attr("disabled", true);
+}
+
 function modify_perview() {
-	//修改评论模块的perview页面
-	$("#my_preview").find("#id_name").attr("placeholder", "请输入您的昵称");
-	$("#my_preview").find("#id_email").attr("placeholder", "请输入您的邮箱（不会被公开）");
-	$("#my_preview").find("#id_comment").attr("placeholder", "请输入您的评论");
-   $("#my_preview").find("#id_url").parent().remove();
+		//修改评论模块的perview页面
+		$("#my_preview").find("#id_name").attr("placeholder", "请输入您的昵称");
+		$("#my_preview").find("#id_email").attr("placeholder", "请输入您的邮箱（不会被公开）");
+		$("#my_preview").find("#id_comment").attr("placeholder", "请输入您的评论");
+	  $("#my_preview").find("#id_url").parent().remove();
 }
