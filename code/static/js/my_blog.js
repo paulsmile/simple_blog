f4552f86e290fbc9d61d2f415e2174d1c6d8f974
cjyfff <1318434601@qq.com>
2014-05-10 03:08:19 +0800

    整理代码排版

--- code/static/js/my_blog.js
+++ code/static/js/my_blog.js
@@ -1,40 +1,40 @@
 $(document).ready(function (){
-	  var	codeLength = 4;
-		submit_comment();
-		back_to_post();
-		create_code(codeLength);
-	  change_code(codeLength);
-	  control_submit(codeLength);
-	  modify_perview();
-	  initial_blog_page();
+    var	codeLength = 4;
+    submit_comment();
+    back_to_post();
+    create_code(codeLength);
+    change_code(codeLength);
+    control_submit(codeLength);
+    modify_perview();
+    initial_blog_page();
 });
 
 function back_to_post(){
-	//实现返回按钮
-    $("#back_to_post").click(function () {
-        window.history.back();
-        });
+     //实现返回按钮
+     $("#back_to_post").click(function () {
+         window.history.back();
+      });
 }
 
 function submit_comment() {
-		//实现ctrl+enter提交评论
-		$(document).keypress(function e() {
-			if (e.ctrlKey && e.which === 13 || e.which === 10) {
-				$("#comment_from").submit();
-			}
-		});
+    //实现ctrl+enter提交评论
+    $(document).keypress(function e() {
+        if (e.ctrlKey && e.which === 13 || e.which === 10) {
+            $("#comment_from").submit();
+        }
+    });
 }
 
 function change_code(codeLength) {
-		//更改验证码
-		$("#checkCode").click(function () {
-			create_code(codeLength);
-		});
+    //更改验证码
+    $("#checkCode").click(function () {
+        create_code(codeLength);
+    });
 }
 
 var code ;
 function create_code(codeLength){
-	//生成验证码
+    //生成验证码
     code = "";
     var checkCode = $("#checkCode");
     var selectChar = new Array(0,1,2,3,4,5,6,7,8,9,'A','B','C','D','E','F','G','H','I','J','K','L',
@@ -44,7 +44,6 @@ function create_code(codeLength){
         var charIndex = Math.floor(Math.random()*36);
         code +=selectChar[charIndex];
     }
-
     if(checkCode){
         checkCode.addClass("code");
         checkCode.val(code);
@@ -52,7 +51,7 @@ function create_code(codeLength){
 }
 
 function verify_code (){
-	//验证验证码
+    //验证验证码
     var user_input_code = $("#user_input_code").val().toLowerCase().trim();
     if(user_input_code.length <=0){
         return false;
@@ -63,33 +62,33 @@ function verify_code (){
 }
 
 function control_submit(codeLength) {
-	//当验证码验证成功后，才允许提交评论
+    //当验证码验证成功后，才允许提交评论
     $("#user_input_code").keyup(function () {
-    	var code_len = $("#user_input_code").val().trim().length
-    	if (code_len < codeLength) {
-    		  $("#valid_failed").text("");
-    	} else if (code_len > codeLength) {
-    		  $("#valid_failed").text("验证码错误");
-    		  $("#submit_comment").attr("disabled", true);
-    	} else {
-		      if (verify_code()) {
-			       $("#submit_comment").attr("disabled", false);
-	        } else {
-	           $("#valid_failed").text("验证码错误");			       
-	        }
-		  }
+        var code_len = $("#user_input_code").val().trim().length
+            if (code_len < codeLength) {
+                $("#valid_failed").text("");
+            } else if (code_len > codeLength) {
+                $("#valid_failed").text("验证码错误");
+    	  $("#submit_comment").attr("disabled", true);
+           } else {
+                if (verify_code()) {
+                    $("#submit_comment").attr("disabled", false);
+	 } else {
+	      $("#valid_failed").text("验证码错误");			       
+	  }
+           }
     });
 }
 
 function initial_blog_page() {
-	  //防止从评论模块的perview页面后退到blog页面时，评论的提交按钮仍然维持disable=“false”状态
-	  $("#submit_comment").attr("disabled", true);
+    //防止从评论模块的perview页面后退到blog页面时，评论的提交按钮仍然维持disable=“false”状态
+    $("#submit_comment").attr("disabled", true);
 }
 
 function modify_perview() {
-		//修改评论模块的perview页面
-		$("#my_preview").find("#id_name").attr("placeholder", "请输入您的昵称");
-		$("#my_preview").find("#id_email").attr("placeholder", "请输入您的邮箱（不会被公开）");
-		$("#my_preview").find("#id_comment").attr("placeholder", "请输入您的评论");
-	  $("#my_preview").find("#id_url").parent().remove();
+    //修改评论模块的perview页面
+    $("#my_preview").find("#id_name").attr("placeholder", "请输入您的昵称");
+    $("#my_preview").find("#id_email").attr("placeholder", "请输入您的邮箱（不会被公开）");
+    $("#my_preview").find("#id_comment").attr("placeholder", "请输入您的评论");
+    $("#my_preview").find("#id_url").parent().remove();
 }
