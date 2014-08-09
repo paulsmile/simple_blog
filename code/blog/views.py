c8be7882f37d5985149ccef1fbd7985aad138c18
cjyfff <1318434601@qq.com>
2014-08-10 03:16:30 +0800

    refactor views into class base view

--- code/blog/views.py
+++ code/blog/views.py
@@ -1,168 +1,34 @@
+#! /usr/bin/env python
 #coding=utf-8
-from django.shortcuts import render
-from blog.models import BlogPost, Tag
-from django.http import Http404, HttpResponseRedirect
-from django.views.decorators.cache import cache_page
-from django.views.decorators.cache import cache_control
-from django.views.decorators.cache import never_cache
+
+from django.shortcuts import get_object_or_404
 from django.contrib.syndication.views import Feed
-from django.utils.functional import cached_property
+from django.views.generic.base import View, ContextMixin, RedirectView
+from django.views.generic import TemplateView
+from django.core.urlresolvers import reverse_lazy
+from django_comments.forms import Comment
+
+from blog.models import BlogPost, Tag
 
 
-@cache_page(60 * 15)
-@cache_control(public=True, must_revalidate=True, max_age=1200)
-def index_page(request):
-    '''Index page view, this view also implement paginator function.'''
-    from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-
-    RANGE_MAX = 5
-    posts = BlogPost.objects.all()
-    paginator = Paginator(posts, 5)
-    page = request.GET.get('page')
-
-    try:
-        posts_of_page = paginator.page(page)
-    except PageNotAnInteger:
-        posts_of_page = paginator.page(1)
-    except EmptyPage:
-        posts_of_page = paginator.page(paginator.num_pages)
-
-    tag_objects = ProcessTag()
-    tag_list = tag_objects.count
-    show_lastest_comment = ShowLastestComment()
-    all_comments_list = show_lastest_comment.show
-
-    page_range = paginator.page_range
-    current_page = posts_of_page.number
-    if len(page_range) > RANGE_MAX:
-        if current_page > RANGE_MAX/2:
-            border = page_range[-1] - current_page
-            if border < RANGE_MAX/2:
-                page_range = page_range[current_page - RANGE_MAX + border:]
-            else:
-                page_range = page_range[current_page - RANGE_MAX/2 - 1:current_page + RANGE_MAX/2 -1]
-        else:
-            page_range = page_range[:RANGE_MAX]
-
-    return render(request, 'index.html', {
-        'posts_of_page': posts_of_page,
-        'tag_list': tag_list,
-        'all_comments_list': all_comments_list,
-        'page_range': page_range,
-        },
-    )
-
-
-def index_page_2(request):
-    '''Redirect '/' request to '/index/'.'''
-
-    return HttpResponseRedirect('/index/')
-
-
-@never_cache
-def show_blog(request, id=''):
-    '''This view is for showing articles of the blog.models.
-    The functions of this views has:
-    1 Showing the paragraphs, pictures and codes order by the their 'sequence'
-      properties which are define in the models.
-    2 Implementing links of the next\previous pages.
-    '''
-
-    def create_post_objects(objects, output_dict):
-        for i in xrange(len(objects)):
-            output_dict[int('%d'%objects[i].sequence)] = objects[i]
-
-    try:
-        post = BlogPost.objects.get(id=id)
-    except BlogPost.DoesNotExist:
-        raise Http404
-    blog_post_list = {}
-
-    photos = post.photo_set.all()
-    codes = post.code_set.all()
-    paragraphs = post.paragraph_set.all()
-
-    create_post_objects(photos, blog_post_list)
-    create_post_objects(codes, blog_post_list)
-    create_post_objects(paragraphs, blog_post_list)
-
-    context_list = []
-    for x in sorted(blog_post_list):
-        context_list.append(blog_post_list[x])
-
-    rs = {}
-    try:
-        next_post = BlogPost.objects.get(id=int(id)+1)
-        rs['next'] = next_post
-    except BlogPost.DoesNotExist:
-        rs['next'] = 0
-    try:
-        pre_post = BlogPost.objects.get(id=int(id)-1)
-        rs['pre'] = pre_post
-    except BlogPost.DoesNotExist:
-        rs['pre'] = 0
-
-    tag_objects = ProcessTag()
-    tag_list = tag_objects.count
-    show_lastest_comment = ShowLastestComment()
-    all_comments_list = show_lastest_comment.show
-
-    return render(
-        request, 'show_blog.html', {
-            'post': post,
-            'context_list': context_list,
-            'next_post': rs['next'],
-            'pre_post': rs['pre'],
-            'tag_list': tag_list,
-            'all_comments_list': all_comments_list,
-        },
-    )
-
-
-class ProcessTag(object):
+class BaseView(ContextMixin, View):
 
     def __init__(self):
+        super(BaseView, self).__init__()
         self.tags = Tag.objects.all()
+        self.all_comments = Comment.objects.all()
 
-    @cached_property
-    def count(self):
-        '''Generating the tag,
-        and counting the amount of the articles of each tag.
-        '''
-        tag_list = {}
+    def count_tags(self):
+        '''Generating the tag, and counting the amount of the articles of each tag.'''
+
+        tags_list = {}
         for tag in self.tags:
             posts_of_tag = tag.blogpost_set.all()
             tag_amount = len(posts_of_tag)
-            tag_list['%s' % tag.tag_name] = tag_amount
-        return tag_list
+            tags_list['%s' % tag.tag_name] = tag_amount
+        return tags_list
 
-    def show(self, request, tag_name):
-        '''Generating the tag's page.'''
-        for tag in self.tags:
-            if tag.tag_name == tag_name:
-                target_tag = tag
-                break
-        target_post = target_tag.blogpost_set.all()
-        tag_list = self.count
-        show_lastest_comment = ShowLastestComment()
-        all_comments_list = show_lastest_comment.show
-
-        return render(request, 'tag_page.html', {
-            'target_post': target_post,
-            'tag_name': tag_name,
-            'tag_list': tag_list,
-            'all_comments_list': all_comments_list,
-            }, )
-
-
-class ShowLastestComment(object):
-
-    def __init__(self):
-        from django_comments.forms import Comment
-        self.all_comments = Comment.objects.all()
-
-    @cached_property
-    def show(self):
+    def show_comments(self):
         '''Show the latest comments on the pages'''
         all_comments_list = []
         for comment in self.all_comments:
@@ -182,6 +48,129 @@ class ShowLastestComment(object):
             reverse=True)
         all_comments_list = all_comments_list[:10]
         return all_comments_list
+
+    def get_context_data(self, **kwargs):
+        context = super(BaseView, self).get_context_data(**kwargs)
+        context.update({
+            'tags_list': self.count_tags(),
+            'all_comments_list': self.show_comments(),
+        })
+
+        return context
+
+
+class IndexView(BaseView, TemplateView):
+    template_name = 'index.html'
+
+    def get_context_data(self, **kwargs):
+        from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+        context = super(IndexView, self).get_context_data(**kwargs)
+
+        RANGE_MAX = 5
+        posts = BlogPost.objects.all()
+        paginator = Paginator(posts, 5)
+        page = self.request.GET.get('page')
+
+        try:
+            posts_of_page = paginator.page(page)
+        except PageNotAnInteger:
+            posts_of_page = paginator.page(1)
+        except EmptyPage:
+            posts_of_page = paginator.page(paginator.num_pages)
+
+        page_range = paginator.page_range
+        current_page = posts_of_page.number
+        if len(page_range) > RANGE_MAX:
+            if current_page > RANGE_MAX/2:
+                border = page_range[-1] - current_page
+                if border < RANGE_MAX/2:
+                    page_range = page_range[current_page - RANGE_MAX + border:]
+                else:
+                    page_range = page_range[current_page - RANGE_MAX/2 - 1:current_page + RANGE_MAX/2 -1]
+            else:
+                page_range = page_range[:RANGE_MAX]
+
+        context.update({
+            'posts_of_page': posts_of_page,
+            'page_range': page_range,
+        })
+
+        return context
+
+
+class IndexRedirectView(RedirectView):
+    '''Redirect '/' request to '/index/'.'''
+
+    url = reverse_lazy('index_page')
+
+
+class ShowBlogView(BaseView, TemplateView):
+    template_name = 'show_blog.html'
+
+    def create_post_objects(self, objects, output_dict):
+        for i in xrange(len(objects)):
+            output_dict[int('%d'%objects[i].sequence)] = objects[i]
+
+    def get_context_data(self, **kwargs):
+        context = super(ShowBlogView, self).get_context_data(**kwargs)
+
+        id = kwargs['id']
+
+        post = get_object_or_404(BlogPost, id=id)
+
+        blog_post_list = {}
+        photos = post.photo_set.all()
+        codes = post.code_set.all()
+        paragraphs = post.paragraph_set.all()
+
+        # Showing the paragraphs, pictures and codes order by the their 'sequence' properties
+        # which are define in the models:
+        self.create_post_objects(photos, blog_post_list)
+        self.create_post_objects(codes, blog_post_list)
+        self.create_post_objects(paragraphs, blog_post_list)
+
+        context_list = []
+        for x in sorted(blog_post_list):
+            context_list.append(blog_post_list[x])
+
+        # Implementing links of the next\previous pages:
+        rs = {}
+        try:
+            next_post = BlogPost.objects.get(id=int(id)+1)
+            rs['next'] = next_post
+        except BlogPost.DoesNotExist:
+            rs['next'] = 0
+        try:
+            pre_post = BlogPost.objects.get(id=int(id)-1)
+            rs['pre'] = pre_post
+        except BlogPost.DoesNotExist:
+            rs['pre'] = 0
+
+        context.update({
+            'post': post,
+            'context_list': context_list,
+            'next_post': rs['next'],
+            'pre_post': rs['pre'],
+        })
+        return context
+
+
+class ShowTagView(BaseView, TemplateView):
+    '''Generating the tag's page.'''
+    template_name = 'tag_page.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(ShowTagView, self).get_context_data(**kwargs)
+        tag_name = kwargs['tag_name']
+        tags = Tag.objects.all()
+        for tag in tags:
+            if tag.tag_name == tag_name:
+                target_tag = tag
+                break
+        target_post = target_tag.blogpost_set.all()
+
+        context.update({'target_post': target_post, 'tag_name': tag_name, })
+        return context
 
 
 class RSSFeed(Feed):
--- code/simple_blog/urls.py
+++ code/simple_blog/urls.py
@@ -1,23 +1,23 @@
 #coding=utf-8
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-from blog.views import index_page, index_page_2, show_blog, RSSFeed,\
-ProcessTag
+from blog.views import RSSFeed, IndexView, IndexRedirectView, ShowBlogView, ShowTagView
 from weixin.views import weixin_api
 from django.conf import settings
 
 admin.autodiscover()
-process_tag = ProcessTag()
-show_tag_page = process_tag.show
 
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
+
+    url(r'^index/$', IndexView.as_view(), name='index_page'),
+    url(r'^$', IndexRedirectView.as_view(), name='index_redirect'),
+    url(r'^blog/(?P<id>\d+)/$', ShowBlogView.as_view(), name='detail_blog'),
+    url(r'^tag_page/(?P<tag_name>.+)/$', ShowTagView.as_view(), name='tag_page'),
     url(r'^comments/', include('django_comments.urls')),
-    url(r'^index/$', index_page, name='index_page'),
-    url(r'^$', index_page_2),
-    url(r'^blog/(?P<id>\d+)/$', show_blog, name='detail_blog'),
+
     url(r'^latest/feed/$', RSSFeed(), name='RSS_url'),
-    url(r'^tag_page/(?P<tag_name>.+)/$', show_tag_page, name='tag_page'),
+
     url(r'^weixin/$', weixin_api, name='weixin_api'),
 )
 
